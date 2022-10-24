6e46241a72fc68479914ad60eeaa5a6270466e99
Ducht <ducht@smartosc.com>
2022-10-24 08:53:34 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<!-- @format -->
+
+![Alternative text](https://i.ibb.co/t2LP6tL/Dodgem.jpg)
+
+## Step One
+
+# If you don't have Nodejs, Yarn Environment
+
+Try install Nodejs by access link [Here](https://nodejs.org/en/download/).
+Try install Yarn by access link [Here](https://classic.yarnpkg.com/lang/en/docs/install/#windows-stable).
+
+## Step Two
+
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+# AI_DODGEM
---
+++ src/App.tsx
@@ -0,0 +1,219 @@
+/** @format */
+
+import clsx from "clsx";
+import React, { useCallback, useEffect, useState } from "react";
+import Board from "./Board";
+import { Player } from "./const";
+import { game } from "./Helper/Game";
+import "./scss/App.scss";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import axios from "axios";
+
+export const BOARD_INIT = [
+	[Player.RIM, Player.RIM, Player.RIM, Player.RIM, Player.RIM],
+	[Player.RIM, Player.USER, Player.NONE, Player.NONE, Player.RIM],
+	[Player.RIM, Player.USER, Player.NONE, Player.NONE, Player.RIM],
+	[Player.RIM, Player.NONE, Player.COMPUTER, Player.COMPUTER, Player.RIM],
+	[Player.RIM, Player.RIM, Player.RIM, Player.RIM, Player.RIM],
+];
+
+export const White_Value = [
+	[0, 85, 90, 100, 0],
+	[0, 30, 35, 40, 0],
+	[0, 15, 20, 25, 0],
+	[0, 0, 5, 10, 0],
+	[0, 0, 0, 0, 0],
+];
+
+export const Black_Value = [
+	[0, 0, 0, 0, 0],
+	[0, -10, -25, -40, -60],
+	[0, -5, -20, -35, -50],
+	[0, 0, -15, -30, -45],
+	[0, 0, 0, 0, 0],
+];
+
+export type CurrentUserPlay = Player.COMPUTER | Player.USER;
+
+interface AppContextType {
+	currentUserPlay: Player;
+	currentSelectPosition: number[];
+	boardState: any;
+	setCurrentUserPlay: () => void;
+	userMove: (toX: number, toY: number) => void;
+	changeCurrentSelectPosition: (x: number, y: number) => void;
+	canLegalMove: (toX: number, toY: number, player: Player) => boolean;
+}
+
+export const AppContext = React.createContext<AppContextType>({
+	currentUserPlay: Player.USER,
+	currentSelectPosition: [] as number[],
+	boardState: [],
+	setCurrentUserPlay: () => {},
+	userMove: (toX: number, toY: number) => {},
+	changeCurrentSelectPosition: (x: number, y: number) => {},
+	canLegalMove: (toX: number, toY: number, player: Player) => true,
+});
+
+const move = [];
+
+function App() {
+	const [currentUserPlay, setCurrentUserPlay] = useState<CurrentUserPlay>(
+		Player.COMPUTER
+	);
+	const [currentSelectPosition, setCurrentSelectPosition] = useState<number[]>(
+		[]
+	);
+	const [boardState, setBoardState] = useState(BOARD_INIT);
+	const [level, setLevel] = useState<number>(3);
+	const [isStart, setStart] = useState(false);
+	const [winner, setWinner] = useState<Player | null>(null);
+
+	const changeUserPlay = useCallback(() => {
+		setCurrentUserPlay(
+			currentUserPlay === Player.COMPUTER ? Player.USER : Player.COMPUTER
+		);
+	}, [currentUserPlay]);
+
+	//move point
+	const userMove = useCallback(
+		(toX, toY) => {
+			//Check if place dont have point to set new position of point select
+			if (
+				(boardState[toX][toY] === Player.NONE ||
+					boardState[toX][toY] === Player.RIM) &&
+				currentSelectPosition.length > 0
+			) {
+				const currentBoardValue = [...boardState];
+				currentBoardValue[currentSelectPosition[0]][
+					currentSelectPosition[1]
+				] = 0;
+				currentBoardValue[toX][toY] = currentUserPlay;
+				setBoardState(currentBoardValue);
+
+				if (game.isWinning(Player.USER)) {
+					userWinning(Player.USER);
+					return;
+				}
+				move.push(currentBoardValue);
+				setCurrentSelectPosition([]);
+				changeUserPlay();
+				// game.getBestMovePoint(
+				// 	boardState,
+				// 	Player.USER,
+				// 	(player: Player | null) => userWinning(player)
+				// );
+			}
+		},
+		[boardState, changeUserPlay, currentSelectPosition, currentUserPlay]
+	);
+
+	//check can move point
+	const canMovePoint = useCallback(
+		(toX, toY, player) => {
+			const [cX, cY] = currentSelectPosition;
+			const dx = toX - cX;
+			const dy = toY - cY;
+
+			if (currentUserPlay === Player.USER) {
+				return (
+					(Math.abs(dx) === 1 && Math.abs(dy) === 0) ||
+					(Math.abs(dx) === 0 && dy === 1)
+				);
+			} else {
+				return (
+					(dx === -1 && Math.abs(dy) === 0) ||
+					(Math.abs(dx) === 0 && Math.abs(dy) === 1)
+				);
+			}
+		},
+		[currentSelectPosition, currentUserPlay]
+	);
+
+	const changeCurrentSelectPosition = useCallback((x, y) => {
+		setCurrentSelectPosition([x, y]);
+	}, []);
+
+
+	const setLevelGame = useCallback((value: number) => {
+		setLevel(value);
+	}, []);
+
+	const userWinning = (player: Player | null) => {
+		if (player) {
+			setWinner(player);
+		}
+	};
+
+	useEffect(() => {
+		if (currentUserPlay === Player.COMPUTER && isStart) {
+			console.log('hello')
+			setBoardState(
+				game.getBestMovePoint(
+					boardState,
+					Player.COMPUTER,
+					(player: Player | null) => userWinning(player)
+				)
+			);
+			changeUserPlay();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [boardState, changeUserPlay, currentUserPlay, isStart]);
+
+	useEffect(() => {
+		setStart(false);
+		setBoardState(BOARD_INIT);
+		setCurrentUserPlay(Player.COMPUTER);
+		game.setLevel(level);
+		setWinner(null);
+	}, [level, setLevelGame])
+
+	useEffect(() => {
+		if(!isStart) {
+			setBoardState(BOARD_INIT);
+			setCurrentUserPlay(Player.COMPUTER);
+			setWinner(null);
+		}
+	}, [isStart])
+	return (
+		<>
+			<AppContext.Provider
+				value={{
+					currentUserPlay,
+					currentSelectPosition,
+					boardState: boardState,
+					setCurrentUserPlay: changeUserPlay,
+					userMove: userMove,
+					changeCurrentSelectPosition: changeCurrentSelectPosition,
+					canLegalMove: canMovePoint,
+				}}>
+				<div className='app__container'>
+					<Board
+						boardState={boardState}
+						setStart={setStart}
+						setLevel={setLevelGame}
+						isStart={isStart}
+						level={level}
+					/>
+
+					<div>
+						{winner && (
+							<pre className='app__winner'>
+								{`Quân   `}
+								<div
+									className={`${clsx(
+										"square__wrapper",
+										winner === Player.COMPUTER && "square__computer",
+										winner === Player.USER && "square__user"
+									)}`}></div>
+								{`   thắng`}
+							</pre>
+						)}
+					</div>
+				</div>
+			</AppContext.Provider>
+		</>
+	);
+}
+
+export default App;
